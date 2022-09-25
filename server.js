4a6d00ad0d33117a769e3537f3d66e9cc3e4d1b1
GrahamA4 <abraham3105schl@gmail.com>
2022-09-24 21:06:43 -0400

    created server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const PORT = 3000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`express Server listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('juby');
+});
