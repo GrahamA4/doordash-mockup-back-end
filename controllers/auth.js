b9778890a2c143fa1f7fb706cf6b217ef544aeea
GrahamA4 <abraham3105schl@gmail.com>
2022-10-27 16:18:53 -0400

    userInsert patch test

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,7 +22,7 @@ export const signUp = async (req, res) => {
       password_digest
     });
 
-    await user.save();
+    await db.users.insertOne(user);
 
     const payload = {
       id: user._id,
