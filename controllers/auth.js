d76f5d763532a6345f0dac1b1a7ace8572f483c3
GrahamA4 <abraham3105schl@gmail.com>
2022-11-30 15:20:27 -0500

    insertOne() for saving user info Test 15

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,6 +22,7 @@ export const signUp = async (req, res) => {
       password_digest
     });
 
+    console.log(user);
     await user.save().then(() => console.log('working'));
 
     const payload = {
