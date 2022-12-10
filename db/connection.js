ee16d8173b06736e1cb983a94b515e13586224d8
GrahamA4 <abraham3105schl@gmail.com>
2022-12-10 17:08:46 -0500

    insertOne() for saving user info Test 20

--- db/connection.js
+++ db/connection.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-let MONGODB_URI = process.env.PROD_MONGODB;
-console.log(process.env.PROD_MONGODB);
+// let MONGODB_URI = process.env.PROD_MONGODB;
+// console.log(process.env.PROD_MONGODB);
 mongoose
-  .connect(MONGODB_URI)
+  .connect('mongodb+srv://agraham4:agraham4@cluster0.uadq7nq.mongodb.net/test')
   .catch((error) =>
     console.error('Error connecting to MongoDB', error.message)
   );
