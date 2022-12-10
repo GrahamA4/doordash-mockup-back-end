56f438911867adb622f6e9afc592161df0d8165b
GrahamA4 <abraham3105schl@gmail.com>
2022-12-10 16:52:40 -0500

    insertOne() for saving user info Test 18

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ dotenv.config();
 let MONGODB_URI = process.env.PROD_MONGODB;
 console.log(MONGODB_URI);
 mongoose
-  .connect(MONGODB_URI)
+  .connect(`${MONGODB_URI}`)
   .then(() => console.log('connected successfully'))
   .catch((error) =>
     console.error('Error connecting to MongoDB', error.message)
