0ea5339db09906eb1ec619da94607be2c1becd2c
GrahamA4 <abraham3105schl@gmail.com>
2022-12-27 18:53:55 -0500

    creates cors connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes/index.js';
+import cors from 'cors';
 import db from './db/connection.js';
 
 dotenv.config();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', routes);
 
 app.get('/', (req, res) => {
